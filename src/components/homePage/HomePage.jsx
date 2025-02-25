8088221e488ede9838d0e8f2d1b9fc8299845c37
david <dahodprod@gmail.com>
2025-02-25 16:45:29 +0100

    add username to homepage

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -5,15 +5,22 @@ import "./home.css";
 
 const HomePage = () => {
   const [activate, setActivate] = useState(false);
+  const [userName, setUserName] = useState("");
 
   useEffect(() => {
     const isConnected = localStorage.getItem("blog_user_isActive");
+    const user = JSON.parse(localStorage.getItem("blog_user"));
+    console.log(user);
+
     setActivate(isConnected);
-  });
+    if (user) {
+      setUserName(user.username);
+    }
+  }, []);
 
   return (
     <>
-      <h2> Bienvenue sur notre blog !</h2>
+      <h2> Bienvenue sur notre blog {userName ? `#${userName}` : ""} !</h2>
       <div className="homePage_button_container">
         <NavLink
           to="/login"
